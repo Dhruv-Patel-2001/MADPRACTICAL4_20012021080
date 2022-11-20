434c0cb0d7fa2b0d9fd660d679cc61d0c42ab21b
Dhruv Patel <dhruvpatel20@gnu.ac.in>
2022-11-20 21:23:26 +0530

    Practical4 completed...

---
+++ app/src/main/java/com/example/madpractical4_20012021080/SecondActivity.kt
@@ -0,0 +1,11 @@
+package com.example.madpractical4_20012021080
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
